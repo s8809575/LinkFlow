869ab1d4b9b3c7576e0c0b9eb6b9c6075ef33c9e
s8809575 <3590713759@qq.com>
2026-06-08 19:33:48 +0800

    删除旧版本和其他冗余文件

--- android/app/src/main/java/com/linkflow/app/util/PairingContextDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.linkflow.app.util
-
-data class PairingContextDTO(
-    val schemaVersion: String = "1.0.0",
-    val hostIpv4: String,
-    val port: Int,
-    val pairingId: String,
-    val keyB64: String
-)
